fafa19281751b9acbbf16a31c492e32e167e0655
BraianGimenez <brazz3013@gmail.com>
2022-12-05 20:36:01 -0300

    Agregando archivos a repositorio

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
-app.use(express.static("public/img"));
+app.use(express.static("public"));
 
 app.get("/",(req,res) => {
     return res.sendFile(path.join(__dirname,"DH-Heroes/views/index.html"))
